15879139d65202671b461f2f71b5c732f408f5fc
Cuong <121237768+NMCuonG08@users.noreply.github.com>
2025-02-23 23:32:59 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const store = new MongoDBStore({
 
 
 app.use(cors({
-    origin: [ 'http://localhost:5173','https://quizze-2j60.onrender.com'], 
+    origin: [ 'http://localhost:5173','https://webmern-nmcuong08s-projects.vercel.app'], 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     optionsSuccessStatus: 200
 }));
